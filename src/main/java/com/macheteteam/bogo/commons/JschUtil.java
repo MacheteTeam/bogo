7097606e7a2fbf104e172a3f7b95b8f2da1729d9
aCoderx <1121535770@qq.com>
2017-08-20 22:23:46 +0800

    jsch scp文件上传

--- src/main/java/com/macheteteam/bogo/commons/JschUtil.java
+++ src/main/java/com/macheteteam/bogo/commons/JschUtil.java
@@ -5,9 +5,7 @@ import com.macheteteam.bogo.utils.ConfigHelp;
 import com.macheteteam.bogo.utils.StringUtils;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.function.Consumer;
 
 /**
@@ -39,6 +37,68 @@ public class JschUtil {
         }
     }
 
+    /**
+     * scp 文件上传
+     * @param localFile 本地文件路径
+     * @param remoteFile 目标文件路径
+     */
+    public static void scp(String localFile, String remoteFile){
+        if (StringUtils.isNotEmpty(localFile,remoteFile)) {
+            Session session = getSession();
+            try {
+                Channel channel = session.openChannel("exec");
+                String command = "scp -t " + remoteFile;
+                ((ChannelExec) channel).setCommand(command);
+                channel.connect();
+                OutputStream out = channel.getOutputStream();
+                InputStream in = channel.getInputStream();
+
+                if(checkAck(in)!=0){
+                    System.exit(0);
+                }
+
+                File _lfile = new File(localFile);
+
+                long filesize=_lfile.length();
+                command="C0644 "+filesize+" ";
+                if(localFile.lastIndexOf('/')>0){
+                    command+=localFile.substring(localFile.lastIndexOf('/')+1);
+                }
+                else{
+                    command+=localFile;
+                }
+                command+="\n";
+                out.write(command.getBytes()); out.flush();
+
+                if(checkAck(in)!=0){
+                    System.exit(0);
+                }
+
+                FileInputStream fis;
+                fis = new FileInputStream(_lfile);
+                byte[] buf = new byte[1024];
+                while (true) {
+                    int len = fis.read(buf, 0, buf.length);
+                    if (len <= 0) break;
+                    out.write(buf, 0, len); //out.flush();
+                }
+                fis.close();
+                // send '\0'
+                buf[0] = 0;
+                out.write(buf, 0, 1);
+                out.flush();
+                if(checkAck(in)!=0){
+                    System.exit(0);
+                }
+                out.close();
+                channel.disconnect();
+                session.disconnect();
+            } catch (JSchException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     private static Session getSession(){
         Session session = null;
         try {
@@ -57,6 +117,32 @@ public class JschUtil {
             e.printStackTrace();
         }
         return session;
+    }
+
+    private static int checkAck(InputStream in) throws IOException{
+        int b=in.read();
+        // b may be 0 for success,
+        //          1 for error,
+        //          2 for fatal error,
+        //          -1
+        if(b==0) return b;
+        if(b==-1) return b;
+
+        if(b==1 || b==2){
+            StringBuffer sb=new StringBuffer();
+            int c;
+            do {
+                c=in.read();
+                sb.append((char)c);
+            } while(c!='\n');
+            if(b==1){ // error
+                System.out.print(sb.toString());
+            }
+            if(b==2){ // fatal error
+                System.out.print(sb.toString());
+            }
+        }
+        return b;
     }
 
 }
